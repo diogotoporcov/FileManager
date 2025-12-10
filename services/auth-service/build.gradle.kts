b9441527044349478721b56500771f461b56a32a
Diogo Toporcov <diogotoporcov@gmail.com>
2025-12-10 19:42:13 -0300

    fix(auth-service): correct incorrectly typed dependency

--- services/auth-service/build.gradle.kts
+++ services/auth-service/build.gradle.kts
@@ -18,32 +18,12 @@ repositories {
     mavenCentral()
 }
 
-plugins {
-    java
-    id("org.springframework.boot") version "4.0.0"
-    id("io.spring.dependency-management") version "1.1.7"
-}
-
-group = "com.diogotoporcov"
-version = "0.0.1-SNAPSHOT"
-description = "auth-service"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
-    }
-}
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-jose")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
@@ -61,12 +41,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
-
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
 
 
 tasks.withType<Test> {
