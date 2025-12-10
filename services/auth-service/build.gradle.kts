a0b25b887478c0c88ee774622cfd5fd4623de621
Diogo Toporcov <diogotoporcov@gmail.com>
2025-12-10 19:23:05 -0300

    add missing gradle files

---
+++ services/auth-service/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.diogotoporcov"
+version = "0.0.1-SNAPSHOT"
+description = "auth-service"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.diogotoporcov"
+version = "0.0.1-SNAPSHOT"
+description = "auth-service"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
